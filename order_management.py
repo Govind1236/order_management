489aa7482c72e8c03363f0123d0229addb809689
Govind Basnet <govindbasnet02@gmail.com>
2024-12-25 20:08:32 +0545

    user data input

--- order_management.py
+++ order_management.py
@@ -11,13 +11,13 @@ print("Welcome to my cafe right ")
 print("Here is the menu of our cafe: ")
 print("Burger: 80\nTea: 40\nOrange Juice: 120\nPizza: 500\nDrinks: 80\n")
 order_total = 0
-item_x = input("Enter The Order You Want to Place = ")
+item_x = input("Enter The Order You Want to Place = ").title()
 if item_x in menu:
     order_total += menu[item_x]
     print(order_total)
 else:
     print (f"Item You Order {item_x} is not available ")
-another_order = input("Do you want To Place Order Again? (yes/No) ")
+another_order = input("Do you want To Place Order Again? (yes/No) ").title()
 item_y = input("Enter the name of item you want to add =")
 if another_order == "yes":
     order_total += menu[item_y]
---
+++ pra.py
@@ -0,0 +1,8 @@
+menu = []
+print("Add Item To list ")
+item_list = input("Enter the name of Item: ")
+total = 0
+for item in menu:
+    total += menu[item_list]
+    print("The Order total is ", total)
+    
